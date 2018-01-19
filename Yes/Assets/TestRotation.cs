2bec345fb3dfa2c4969a8274b7f3c3a198ed5248
n8-des <nathan.desjardins@baxter-academy.org>
2018-01-19 11:48:29 -0500

    Goodbye Old Freind
    Only and final version of Salt VR

---
+++ Yes/Assets/TestRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotation : MonoBehaviour {
+
+	public GameObject target;
+	public float speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 move = new Vector3 (Input.GetAxisRaw ("Horizontal"),0, Input.GetAxisRaw ("Vertical"));
+		transform.position += (move * speed);
+	}
+}
